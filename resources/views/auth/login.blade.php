0e7d45419196177a58e6ee1dd55562e64dc7faef
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-07-19 11:36:48 -0500

    Cambio de icons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,9 +5,9 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png')}}">
-  <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png')}}">
+  <link rel="icon" type="image/png" href="https://wtech.com.mx/romeroip/favicon/62d6504cbf0b1ei2.png">
   <title>
-    Argon Dashboard 2 PRO by Creative Tim
+    RomeroIp
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
