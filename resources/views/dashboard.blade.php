8ab572baeaafb1066037dab67abe1d8328eca332
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-07-19 11:04:34 -0500

    Calendar view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,98 +1,207 @@
 @extends('layouts.app')
 
-@section('breadcrumb')
+@section('content')
+
+<div class="row mb-lg-7">
+
+        <div class="col-xl-9">
+          <div class="card card-calendar">
+            <div class="card-body p-3">
+              <div class="calendar" data-bs-toggle="calendar" id="calendar"></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xl-3">
           <div class="row">
-            <div class="col-xl-3 col-md-6">
-              <div class="card card-stats" style="background: {{$configuracion->color_iconos_cards}}">
-                <!-- Card body -->
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">Total traffic</h5>
-                      <span class="h2 font-weight-bold mb-0">350,897</span>
-                    </div>
-                    <div class="col-auto">
-                      <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
-                        <i class="ni ni-active-40"></i>
-                      </div>
-                    </div>
-                  </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
+            <div class="col-xl-12 col-md-6 mt-xl-0 mt-4">
+              <div class="card">
+                <div class="card-header p-3 pb-0">
+                  <h6 class="mb-0">Next events</h6>
                 </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-              <div class="card card-stats" style="background: {{$configuracion->color_iconos_cards}}">
-                <!-- Card body -->
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
-                      <span class="h2 font-weight-bold mb-0">2,356</span>
+                <div class="card-body border-radius-lg p-3">
+                  <div class="d-flex">
+                    <div>
+                      <div class="icon icon-shape bg-danger-soft shadow text-center border-radius-md shadow-none">
+                        <i class="ni ni-money-coins text-lg text-danger text-gradient opacity-10" aria-hidden="true"></i>
+                      </div>
                     </div>
-                    <div class="col-auto">
-                      <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
-                        <i class="ni ni-chart-pie-35"></i>
+                    <div class="ms-3">
+                      <div class="numbers">
+                        <h6 class="mb-1 text-dark text-sm">Cyber Week</h6>
+                        <span class="text-sm">27 March 2021, at 12:30 PM</span>
                       </div>
                     </div>
                   </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-              <div class="card card-stats" style="background: {{$configuracion->color_iconos_cards}}">
-                <!-- Card body -->
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">Sales</h5>
-                      <span class="h2 font-weight-bold mb-0">924</span>
+                  <div class="d-flex mt-4">
+                    <div>
+                      <div class="icon icon-shape bg-primary-soft shadow text-center border-radius-md shadow-none">
+                        <i class="ni ni-bell-55 text-lg text-primary text-gradient opacity-10" aria-hidden="true"></i>
+                      </div>
                     </div>
-                    <div class="col-auto">
-                      <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
-                        <i class="ni ni-money-coins"></i>
+                    <div class="ms-3">
+                      <div class="numbers">
+                        <h6 class="mb-1 text-dark text-sm">Meeting with Marry</h6>
+                        <span class="text-sm">24 March 2021, at 10:00 PM</span>
                       </div>
                     </div>
                   </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-              <div class="card card-stats" style="background: {{$configuracion->color_iconos_cards}}">
-                <!-- Card body -->
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">Performance</h5>
-                      <span class="h2 font-weight-bold mb-0">49,65%</span>
+                  <div class="d-flex mt-4">
+                    <div>
+                      <div class="icon icon-shape bg-success-soft shadow text-center border-radius-md shadow-none">
+                        <i class="ni ni-books text-lg text-success text-gradient opacity-10" aria-hidden="true"></i>
+                      </div>
                     </div>
-                    <div class="col-auto">
-                      <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
-                        <i class="ni ni-chart-bar-32"></i>
+                    <div class="ms-3">
+                      <div class="numbers">
+                        <h6 class="mb-1 text-dark text-sm">Book Deposit Hall</h6>
+                        <span class="text-sm">25 March 2021, at 9:30 AM</span>
                       </div>
                     </div>
                   </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
+                  <div class="d-flex mt-4">
+                    <div>
+                      <div class="icon icon-shape bg-warning-soft shadow text-center border-radius-md shadow-none">
+                        <i class="ni ni-delivery-fast text-lg text-warning text-gradient opacity-10" aria-hidden="true"></i>
+                      </div>
+                    </div>
+                    <div class="ms-3">
+                      <div class="numbers">
+                        <h6 class="mb-1 text-dark text-sm">Shipment Deal UK</h6>
+                        <span class="text-sm">25 March 2021, at 2:00 PM</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="d-flex mt-4">
+                    <div>
+                      <div class="icon icon-shape bg-info-soft shadow text-center border-radius-md shadow-none">
+                        <i class="ni ni-palette text-lg text-info text-gradient opacity-10" aria-hidden="true"></i>
+                      </div>
+                    </div>
+                    <div class="ms-3">
+                      <div class="numbers">
+                        <h6 class="mb-1 text-dark text-sm">Verify Dashboard Color Palette</h6>
+                        <span class="text-sm">26 March 2021, at 9:00 AM</span>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
 @endsection
 
-@section('content')
+@section('js_custom')
+    <script !src="">
+    var calendar = new FullCalendar.Calendar(document.getElementById("calendar"), {
+      contentHeight: 'auto',
+      initialView: "dayGridMonth",
+      headerToolbar: {
+        start: 'title', // will normally be on the left. if RTL, will be on the right
+        center: '',
+        end: 'today prev,next' // will normally be on the right. if RTL, will be on the left
+      },
+      selectable: true,
+      editable: true,
+      initialDate: '2021-12-01',
+      events: [{
+          title: 'Call with Dave',
+          start: '2021-11-18',
+          end: '2021-11-18',
+          className: 'bg-gradient-danger'
+        },
 
+        {
+          title: 'Lunch meeting',
+          start: '2021-11-21',
+          end: '2021-11-22',
+          className: 'bg-gradient-warning'
+        },
+
+        {
+          title: 'All day conference',
+          start: '2021-11-29',
+          end: '2021-11-29',
+          className: 'bg-gradient-success'
+        },
+
+        {
+          title: 'Meeting with Mary',
+          start: '2021-12-01',
+          end: '2021-12-01',
+          className: 'bg-gradient-info'
+        },
+
+        {
+          title: 'Winter Hackaton',
+          start: '2021-12-03',
+          end: '2021-12-03',
+          className: 'bg-gradient-danger'
+        },
+
+        {
+          title: 'Digital event',
+          start: '2021-12-07',
+          end: '2021-12-09',
+          className: 'bg-gradient-warning'
+        },
+
+        {
+          title: 'Marketing event',
+          start: '2021-12-10',
+          end: '2021-12-10',
+          className: 'bg-gradient-primary'
+        },
+
+        {
+          title: 'Dinner with Family',
+          start: '2021-12-19',
+          end: '2021-12-19',
+          className: 'bg-gradient-danger'
+        },
+
+        {
+          title: 'Black Friday',
+          start: '2021-12-23',
+          end: '2021-12-23',
+          className: 'bg-gradient-info'
+        },
+
+        {
+          title: 'Cyber Week',
+          start: '2021-12-02',
+          end: '2021-12-02',
+          className: 'bg-gradient-warning'
+        },
+
+      ],
+      views: {
+        month: {
+          titleFormat: {
+            month: "long",
+            year: "numeric"
+          }
+        },
+        agendaWeek: {
+          titleFormat: {
+            month: "long",
+            year: "numeric",
+            day: "numeric"
+          }
+        },
+        agendaDay: {
+          titleFormat: {
+            month: "short",
+            year: "numeric",
+            day: "numeric"
+          }
+        }
+      },
+    });
+
+    calendar.render();
+
+    </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,7 @@
 </head>
 
 <body class="g-sidenav-show   bg-gray-100">
-  <div class="min-height-300 bg-primary position-absolute w-100"></div>
-
+  <div class="min-height-300  position-absolute w-100" style="background-color: {{$configuracion->color_principal}}!important;"></div>
 
    <!-- Sidenav -->
     @include('layouts.sidebar')
@@ -58,95 +57,21 @@
   <script src="{{ asset('assets/js/core/bootstrap.min.js')}}"></script>
   <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js')}}"></script>
   <script src="{{ asset('assets/js/plugins/smooth-scrollbar.min.js')}}"></script>
+  <script src="{{ asset('assets/js/plugins/fullcalendar.min.js')}}"></script>
   <!-- Kanban scripts -->
   <script src="{{ asset('assets/js/plugins/dragula/dragula.min.js')}}"></script>
   <script src="{{ asset('assets/js/plugins/jkanban/jkanban.js')}}"></script>
   <script src="{{ asset('assets/js/plugins/chartjs.min.js')}}"></script>
-  <script src="../../../assets/js/plugins/multistep-form.js"></script>
+
+    @yield('js_custom')
+
   <script>
-    var ctx1 = document.getElementById("chart-line").getContext("2d");
+            var gradientStroke1 = ctx1.createLinearGradient(0, 230, 0, 50);
 
-    var gradientStroke1 = ctx1.createLinearGradient(0, 230, 0, 50);
+            gradientStroke1.addColorStop(1, 'rgba(94, 114, 228, 0.2)');
+            gradientStroke1.addColorStop(0.2, 'rgba(94, 114, 228, 0.0)');
+            gradientStroke1.addColorStop(0, 'rgba(94, 114, 228, 0)');
 
-    gradientStroke1.addColorStop(1, 'rgba(94, 114, 228, 0.2)');
-    gradientStroke1.addColorStop(0.2, 'rgba(94, 114, 228, 0.0)');
-    gradientStroke1.addColorStop(0, 'rgba(94, 114, 228, 0)');
-    new Chart(ctx1, {
-      type: "line",
-      data: {
-        labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        datasets: [{
-          label: "Mobile apps",
-          tension: 0.4,
-          borderWidth: 0,
-          pointRadius: 0,
-          borderColor: "#5e72e4",
-          backgroundColor: gradientStroke1,
-          borderWidth: 3,
-          fill: true,
-          data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
-          maxBarThickness: 6
-
-        }],
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            display: false,
-          }
-        },
-        interaction: {
-          intersect: false,
-          mode: 'index',
-        },
-        scales: {
-          y: {
-            grid: {
-              drawBorder: false,
-              display: true,
-              drawOnChartArea: true,
-              drawTicks: false,
-              borderDash: [5, 5]
-            },
-            ticks: {
-              display: true,
-              padding: 10,
-              color: '#fbfbfb',
-              font: {
-                size: 11,
-                family: "Open Sans",
-                style: 'normal',
-                lineHeight: 2
-              },
-            }
-          },
-          x: {
-            grid: {
-              drawBorder: false,
-              display: false,
-              drawOnChartArea: false,
-              drawTicks: false,
-              borderDash: [5, 5]
-            },
-            ticks: {
-              display: true,
-              color: '#ccc',
-              padding: 20,
-              font: {
-                size: 11,
-                family: "Open Sans",
-                style: 'normal',
-                lineHeight: 2
-              },
-            }
-          },
-        },
-      },
-    });
-  </script>
-    <script>
             if (document.getElementById('edit-deschiption')) {
             var quill = new Quill('#edit-deschiption', {
                 theme: 'snow' // Specify theme in configuration
@@ -198,15 +123,15 @@
                 false,
             );
             }
-    </script>
-  <script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-      var options = {
-        damping: '0.5'
-      }
-      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
+
+        var win = navigator.platform.indexOf('Win') > -1;
+        if (win && document.querySelector('#sidenav-scrollbar')) {
+          var options = {
+            damping: '0.5'
+          }
+          Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+        }
+
   </script>
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
