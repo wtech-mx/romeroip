d0f33448135673b2a72884ed03fc63e6c65bcf54
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-07-19 11:30:54 -0500

    redireciionamiento ligas

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -38,7 +38,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link {{ (Request::is('trademark*') ? 'active' : '') }}" href="romeroip/trademark" target="">
+          <a class="nav-link {{ (Request::is('trademark*') ? 'active' : '') }}" href="https://wtech.com.mx/romeroip/trademark" target="">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
              <i class="fa fa-user-md text-sm opacity-10" style="color: {{$configuracion->color_iconos_sidebar}}"></i>
             </div>
